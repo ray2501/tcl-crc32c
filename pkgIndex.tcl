9253227b6bae265b84067462be6b1eb8af0b6f89
Danilo Chang <ray2501@gmail.com>
2023-12-13 13:56:55 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded crc32c 0.1 \
-    [list load [file join $dir libcrc32c0.1.so] crc32c]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded crc32c 0.1 \
+	    [list load [file join $dir libtcl9crc32c0.1.so] [string totitle crc32c]]
+} else {
+    package ifneeded crc32c 0.1 \
+	    [list load [file join $dir libcrc32c0.1.so] [string totitle crc32c]]
+}
